2fa4613f0ddace6819e19da2352283c0a09e3fe8
Jason Sugata <jasonsugata@gmail.com>
2020-06-14 14:38:09 -0400

    displaying working links to all pages on homepage

--- routes/wiki.js
+++ routes/wiki.js
@@ -5,9 +5,13 @@ const { addPage, editPage, main, userList, userPages, wikiPage } = require('../v
 const { Page } = require('../models')
 
 
-router.get('/', (req, res, next) => {
-  //const pages = Page.findAll()
-  res.send("got to /wiki")
+router.get('/', async (req, res, next) => {
+  try {
+    const pages = await Page.findAll()
+    res.send(main(pages))
+  } catch (error) {
+    next(error)
+  }
 })
 
 
@@ -16,7 +20,7 @@ router.post('/', async (req, res, next) => {
   const page = new Page(req.body)
   try {
     await page.save();
-    res.redirect('/');
+    res.redirect(`/wiki/${page.slug}`);
 
 
   } catch (error) {
@@ -30,6 +34,22 @@ router.post('/', async (req, res, next) => {
 //retreives the "add page" form
 router.get('/add', (req, res, next) => {
   res.send(addPage())
+})
+
+
+// retrieves the specfic created page
+router.get('/:slug', async (req, res, next) => {
+  try {
+    const page = await Page.findOne({
+      where: {
+        slug: req.params.slug
+      }
+    })
+    res.send(wikiPage(page))
+  } catch (error) {
+    next(error)
+  }
+
 })
 
 module.exports = router
